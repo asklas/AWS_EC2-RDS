979cc2ab359eb798d831c6e270c080f332a0e654
asklas <asklas+jyjy0372@gmail.com>
2024-11-12 16:46:32 +0900

    CI/CD

--- src/main/java/org/awstest/AwsTestApplication.java
+++ src/main/java/org/awstest/AwsTestApplication.java
@@ -2,8 +2,10 @@ package org.awstest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/application-dev.properties")
 public class AwsTestApplication {
 
 	public static void main(String[] args) {
